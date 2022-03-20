8310477146a0c732d117e9faf61e4eccdee0b174
Alexander Korostelev <elsit-tm@yandex.ru>
2022-03-20 20:49:19 +0300

    Add test script

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,5 @@
 import { expect } from 'chai';
 
-// import { hello } from '../src';
-
 describe('Typescript + Babel usage suite', () => {
   it('should return string correctly', () => {
     expect('Hello mocha').to.equal('Hello mocha');
