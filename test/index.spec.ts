348bcec625579e66d2e73f17d103dc6b3b1c6866
Alexander Korostelev <elsit-tm@yandex.ru>
2022-03-20 23:57:48 +0300

    Try to set test config

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,19 @@
 import { expect } from 'chai';
+// var jsdom = require('mocha-jsdom')
 
 describe('Typescript + Babel usage suite', () => {
   it('should return string correctly', () => {
     expect('Hello mocha').to.equal('Hello mocha');
   });
 });
+
+// describe('Проверяем переходы у Роута', () => {
+//   jsdom();
+//
+//   it('Переход на новую страницу должен менять состояние сущности history', () => {
+//     window.history.pushState({ page: 'login' }, 'Login', '/login');
+//     window.history.pushState({ page: 'register' }, 'Register', '/register');
+//
+//     expect(window.history.length).to.eq(3);
+//   });
+// });
