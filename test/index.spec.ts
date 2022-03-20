1f76ac6fbdedb9d488859439e092e803ce22045b
Alexander Korostelev <elsit-tm@yandex.ru>
2022-03-20 20:19:39 +0300

    Add test support (chai, mocha) and babel dependencies

---
+++ test/index.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+// import { hello } from '../src';
+
+describe('Typescript + Babel usage suite', () => {
+  it('should return string correctly', () => {
+    expect('Hello mocha', 'Hello mocha');
+  });
+});
