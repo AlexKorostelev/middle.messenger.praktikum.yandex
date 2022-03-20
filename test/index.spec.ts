61e9b5eec0b419928d4e7873805820aa07eac1d0
Alexander Korostelev <elsit-tm@yandex.ru>
2022-03-21 01:35:28 +0300

    Correct test script, add http signin test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,19 +1,49 @@
-import { expect } from 'chai';
-// var jsdom = require('mocha-jsdom')
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
 
-describe('Typescript + Babel usage suite', () => {
-  it('should return string correctly', () => {
-    expect('Hello mocha').to.equal('Hello mocha');
+const jsdom = require('jsdom');
+
+const { JSDOM } = jsdom;
+
+chai.use(chaiHttp);
+
+describe('mocha tests', () => {
+  it('test DOM', () => {
+    const dom = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
+    // eslint-disable-next-line no-console
+    console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
   });
 });
 
-// describe('Проверяем переходы у Роута', () => {
-//   jsdom();
-//
+// describe('Тест страницы чатов', () => {
 //   it('Переход на новую страницу должен менять состояние сущности history', () => {
-//     window.history.pushState({ page: 'login' }, 'Login', '/login');
-//     window.history.pushState({ page: 'register' }, 'Register', '/register');
+//     const dom = new JSDOM('test', {
+//       url: 'http://localhost:1234/messages',
+//       referrer: 'http://localhost:1234/messages',
+//       contentType: 'text/html',
+//       includeNodeLocations: true,
+//       storageQuota: 10000000,
+//     });
 //
-//     expect(window.history.length).to.eq(3);
+//     JSDOM.fromURL('http://localhost:1234/messages', {}).then((dom) => {
+//       console.log(dom.serialize());
+//     });
 //   });
 // });
+
+describe('/POST signin', () => {
+  it('Проверка роута авторизации', (done) => {
+    const payload = {
+      login: 'Alex1985',
+      password: 'Alexander1985!',
+    };
+    chai
+      .request('https://ya-praktikum.tech/api/v2/auth')
+      .post('/signin')
+      .send(payload)
+      .end((_, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
