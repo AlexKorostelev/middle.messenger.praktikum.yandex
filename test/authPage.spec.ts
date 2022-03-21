e06ba612fa78c85ff833ae55a1c3514de21c8c59
Alexander Korostelev <elsit-tm@yandex.ru>
2022-03-21 16:03:10 +0300

    Add authPage test (not work: global.document empty)

--- test/authPage.spec.ts
+++ test/authPage.spec.ts
@@ -1,25 +1,16 @@
-import { registerComponent } from '../src/common/registerComponent';
-import Input from '../src/components/Input';
-import { AuthorizationPage } from '../src/pages/authorization/authorization';
-import Button from '../src/components/Button';
-import InputField from '../src/components/InputField';
-
-const jsdom = require('jsdom');
-
-const { JSDOM } = jsdom;
+// import { registerComponent } from '../src/common/registerComponent';
+// import Input from '../src/components/Input';
+// import { AuthorizationPage } from '../src/pages/authorization/authorization';
+// import Button from '../src/components/Button';
+// import InputField from '../src/components/InputField';
+//
 
 describe('Проверка отрисовки компонентов', () => {
-  beforeEach(() => {
-    registerComponent(Button, 'Button');
-    registerComponent(Input, 'Input');
-    registerComponent(InputField, 'InputField');
-
-    const authPage = new AuthorizationPage();
-    global.document = new JSDOM(authPage.render()).window.document;
-  });
-
   it('Тест компонента AuthorizationPage', () => {
-    const form = global.document.getElementById('authorisation-form');
-    console.log(form);
+    console.log('>>>', global.document.getElementById('app')?.innerText);
+
+    // const authPage = new AuthorizationPage();
+    // const form = global.document.getElementById('app');
+    // console.log('>>>', form);
   });
 });
--- test/mock.js
+++ test/mock.js
@@ -1,1 +1,17 @@
-require.extensions['.less'] = function () { return null; };
+require.extensions['.less'] = function () {
+  return null;
+};
+
+const jsdom = require('jsdom');
+const { registerComponent } = require('../src/common/registerComponent');
+const Button = require('../src/components/Button');
+const Input = require('../src/components/Input');
+const InputField = require('../src/components/InputField');
+
+const { JSDOM } = jsdom;
+
+registerComponent(Button, 'Button');
+registerComponent(Input, 'Input');
+registerComponent(InputField, 'InputField');
+
+global.document = new JSDOM('<div id="app">Hello!</div>').window.document;
